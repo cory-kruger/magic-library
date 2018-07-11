4e63e08fe9c038039d35741c8aa3d3c7df90db39
Cory Kruger <cory@corykruger.ca>
2018-07-11 12:08:10 -0400

    Initial Commit.

---
+++ src/main/java/ca/corykruger/magic/magic_library/App.java
@@ -0,0 +1,13 @@
+package ca.corykruger.magic.magic_library;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
