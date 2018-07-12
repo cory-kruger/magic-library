1d48606d1ea2f78fce8d07b5bc458364fbf4588d
Cory Kruger <cory@corykruger.ca>
2018-07-12 01:28:22 -0400

    Adding JavaDoc

--- src/main/java/ca/corykruger/magic/magic_library/gatherer/DataRetriever.java
+++ src/main/java/ca/corykruger/magic/magic_library/gatherer/DataRetriever.java
@@ -1,23 +1,38 @@
 package ca.corykruger.magic.magic_library.gatherer;
 
-import java.awt.Image;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 
 import javax.imageio.ImageIO;
 
+/**
+ * Retrieves data from the provided URL.
+ */
 public class DataRetriever {
 	
+	/**
+	 * Parameterized website address to retrieve a card's artwork from
+	 */
 	public static final String IMAGE_ADDRESS = "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=%d&type=card";
 	
 	private URL gathererUrl;
 	
+	/**
+	 * Creates a <code>DataRetriever</code> object and specifies the <code>URL</code> to retrieve the data from
+	 * @param gathererURL
+	 */
 	DataRetriever(URL gathererURL) {
 		this.gathererUrl = gathererURL;
 	}
 
-	public Image getImage() throws IOException {
+	/**
+	 * Retrieves the card artwork
+	 * @return The retrieved image
+	 * @throws IOException if unable to retrieve the image
+	 */
+	public BufferedImage getImage() throws IOException {
 		try (InputStream inStream = gathererUrl.openStream();) {
 			return ImageIO.read(inStream);
 		}
--- src/main/java/ca/corykruger/magic/magic_library/gatherer/DataRetrieverFactory.java
+++ src/main/java/ca/corykruger/magic/magic_library/gatherer/DataRetrieverFactory.java
@@ -3,8 +3,16 @@ package ca.corykruger.magic.magic_library.gatherer;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+/**
+ * Constructs a <code>DataRetriever</code> with a ready-to-use <code>URL</code>
+ */
 public class DataRetrieverFactory {
 	
+	/**
+	 * Creates a <code>DataRetriever</code> that can be used to retrieve a card's artwork
+	 * @param multiverseId the UUID of the card to retrieve
+	 * @return A <code>DataRetriever</code> ready to retrieve an image
+	 */
 	public DataRetriever getImageRetriever(int multiverseId) {
 		try {
 			URL gathererUrl = new URL(String.format(DataRetriever.IMAGE_ADDRESS, multiverseId));
